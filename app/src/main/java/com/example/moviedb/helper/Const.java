6707766fbef8c70e3d92adc6a3d136bc968060b2
benedictusardi <benedictusardi1@gmail.com>
2021-10-18 05:45:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/moviedb/helper/Const.java
@@ -0,0 +1,8 @@
+package com.example.moviedb.helper;
+
+public class Const {
+
+    public  static final String API_KEY = "3fdda5647c8cd211ddb1485474b97318";
+    public static final String BASE_URL = "https://api.themoviedb.org/3";
+
+}
---
+++ app/src/main/java/com/example/moviedb/retrofit/ApiEndPoint.java
@@ -0,0 +1,7 @@
+package com.example.moviedb.retrofit;
+
+public interface ApiEndPoint {
+
+
+
+}
